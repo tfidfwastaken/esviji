645079ff41cf0c74510ecd806c434d089148b394
Siddhant <raosiddhant99@gmail.com>
2019-10-01 20:09:53 +0530

    added filter and makefile

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
-a.out: rectangletest.o esviji.o
-	gcc rectangletest.o esviji.o	
+a.out: test.o esviji.o
+	gcc test.o esviji.o	
 esviji.o: esviji.c esviji.h
 	gcc -c esviji.c
-rectangletest.o: rectangletest.c esviji.h 
-	gcc -c rectangletest.c
+test.o: test.c esviji.h 
+	gcc -c test.c
---
+++ test.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "esviji.h"
+
+void drawrectangles();
+
+int main()
+{
+	drawrectangles();
+	return 0;
+}
+
+void drawrectangles()
+{
+	Svg *svg_img;
+	svg_img = svg_create(512, 512);
+	if (svg_img == NULL) {
+		puts("svg_img is NULL");
+	} else {
+		svg_filter(svg_img, "idname", 0, 0, 15);
+		svg_rectangle(svg_img, 256, 256, 0, 0, "yellow", "#000000", 5, 2, 2, "idname");
+		svg_finalize(svg_img);
+		svg_save(svg_img, "rectangle.svg");
+		svg_print(svg_img);
+		svg_free(svg_img);
+	}
+}
+
